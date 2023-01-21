d3797b3d749b9fb28638c99e356e6adb83dd9542
swarup-1 <swarupkadoli9@gmail.com>
2023-01-21 14:50:58 +0530

    change

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-      <ChakraProvider>
+      <ChakraProvider theme={Theme} >
         <App />
       </ChakraProvider>
     </BrowserRouter>
